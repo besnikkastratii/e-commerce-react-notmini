798eeab5874a2149403a9e891394d4572685c20e
besnikkastratii <besnikkastrati2@gmail.com>
2023-05-27 00:34:18 +0200

    u konvertu functions,u shtu pagination edhe search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Switch,Route} from "react-router-dom";
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/Navbar";
-import ProductList from "./components/ProductList";
-import Details from "./components/Details";
-import Cart from "./components/Cart";
-import Default from "./components/Default";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 import Modal from './components/Modal';
 
 function App() {
