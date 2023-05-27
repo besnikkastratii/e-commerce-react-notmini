a68bb595f5873d3fd353ad597ee802be67fc18a1
besnikkastratii <besnikkastrati2@gmail.com>
2023-05-27 21:03:31 +0200

    from detection

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -31,6 +31,7 @@ export default function CartTotals({ value }) {
 
   return (
     <React.Fragment>
+      <form>
       <div className="container">
         <div className="row">
           <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
@@ -93,6 +94,7 @@ export default function CartTotals({ value }) {
           </div>
         </div>
       </div>
+      </form>
     </React.Fragment>
   );
 }
