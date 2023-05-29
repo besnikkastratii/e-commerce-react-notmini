4090019fe149d263a5d1ac1bef1a998ece766242
besnikkastratii <besnikkastrati2@gmail.com>
2023-05-30 01:00:29 +0200

    fixed the glitch on DOM ,Cart Check

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,54 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
-import {ProductConsumer} from '../context';
+import { Link } from 'react-router-dom';
+import { ProductConsumer } from '../context';
 import PropTypes from 'prop-types';
 
 export default class Product extends Component {
-    render() {
-        const {id, title, img, price, inCart} = this.props.product;
-        return (
-            <ProducrWrapper className="col-9 mx-auto col-md-6 col-lg-4 my-3">
-               <div className="card">
-                <ProductConsumer>
-                        {value => (<div className="img-container p-5" onClick={() => value.handleDetail(id)}>
-                            <Link to="/details">
-                                <img src={img} alt="product" className="card-img-top" />
-                            </Link>
-                            <button className="cart-btn" disabled={inCart ? true : false}
-                                onClick={() => {
-                                    value.addToCart(id);
-                                    value.openModal(id);
-                                }}>
-                                {inCart ? (<p className="text-capitalize mb-0" disabled>{""}Product is in Cart</p>)
-                                    : (<i className="fas fa-cart-plus" />)}
-                            </button>
-                        </div>)}
-                </ProductConsumer>
-                 <div className="card-footer d-flex justify-content-between">
-                     <p className="align-self-center mb-0">
-                         {title}
-                     </p>
-                     <h5 className="text-blue font-italic mb-0">
-                         <span className="mr-1">$</span>
-                         {price}
-                     </h5>
-                 </div>
-               </div>
-            </ProducrWrapper>
-        );
-    }
+  render() {
+    const { id, title, img, price } = this.props.product;
+    return (
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-4 my-3">
+        <div className="card">
+          <ProductConsumer>
+            {value => {
+              const isInCart = value.cart.some(item => item.id === id);
+              return (
+                <div className="img-container p-5" onClick={() => value.handleDetail(id)} >
+                  <Link to="/details"> <img src={img} alt="product" className="card-img-top" /> </Link>
+                  <button className="cart-btn" disabled={isInCart} onClick={() => { value.addToCart(id); value.openModal(id); }} >
+                    {isInCart ? ( <p className="mb-0" disabled> Product is in Cart </p> ) : ( <i className="fas fa-cart-plus" /> )}
+                  </button>
+                </div>
+              );
+            }}
+          </ProductConsumer>
+          <div className="card-footer d-flex justify-content-between"> <p className="align-self-center mb-0">{title}</p>
+            <h5 className="text-blue font-italic mb-0"> <span className="mr-1">$</span> {price} </h5>
+          </div>
+        </div>
+      </ProductWrapper>
+    );
+  }
 }
+
 Product.propTypes = {
-    product:PropTypes.shape({
-        id:PropTypes.number,
-        img:PropTypes.string,
-        title:PropTypes.string,
-        price:PropTypes.number,
-        inCart:PropTypes.bool
-    }).isRequired
-}
-const ProducrWrapper =styled.div`
+  product: PropTypes.shape({
+    id: PropTypes.number,
+    img: PropTypes.string,
+    title: PropTypes.string,
+    price: PropTypes.number,
+  }).isRequired,
+};
+
+const ProductWrapper =styled.div`
 .card{
     border-color:tranparent;
     transition:all 1s linear;
