2811e65399bec97e13feebc4051cd99e048d8515
besnikkastratii <besnikkastrati2@gmail.com>
2023-06-03 20:40:58 +0200

    yep :D

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -4,6 +4,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const ImageSlider = ({ images, handleCategoryClick }) => {
+  // eslint-disable-next-line
   const [selectedCategory, setSelectedCategory] = useState(null);
 
   const settings = {
